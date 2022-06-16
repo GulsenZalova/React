3e15ad4b83487f11541db8a899c8d24da8fd2248
GulsenZalova <zalova0202@gmail.com>
2022-06-17 02:09:50 +0400

    update

--- reacttask/newreactproject/src/components/SearchBar.jsx
+++ reacttask/newreactproject/src/components/SearchBar.jsx
@@ -1,11 +1,30 @@
 import React, { Component } from 'react'
 
 export default class SearchBar extends Component {
+
+state={
+  searchQuery:""
+}
+
+stopupdate=(event)=>{
+  event.preventDefault()
+}
   render() {
     return (
-      <div>
-        <h1>SearchBar</h1>
-      </div>
+      <form onSubmit={this.stopupdate}>
+           <div className='form-row mb-5 mt-5'>
+              <div className='col-12'>
+                  <input 
+                  type="text" 
+                  onChange={(event)=> this.setState({searchQuery: event.target.value})}
+                  className='form-control' 
+                   placeholder='Search a movie'
+                   value={this.state.searchQuery}
+                   />
+
+              </div>
+           </div>
+      </form>
     )
   }
 }
