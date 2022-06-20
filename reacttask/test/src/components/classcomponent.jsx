1aed975b03c7456693ae2c8bddb03d78f2d92c6d
GulsenZalova <zalova0202@gmail.com>
2022-06-21 01:50:38 +0400

    update

--- reacttask/test/src/components/classcomponent.jsx
+++ reacttask/test/src/components/classcomponent.jsx
@@ -2,18 +2,31 @@ import React, { Component } from 'react'
 
 export default class Classcomponent extends Component {
   state={
-    counter: 0
+    counter: 0,
+    counter2:0
   } 
+
+componentDidMount(){
+    console.log("component yükləndi")
+}
+componentDidUpdate(prevsProps,prevsState){
+    if(prevsState.counter !== this.state.counter){
+        console.log("component update olundu")
+    }else if(prevsState.counter2 !== this.state.counter2){
+        console.log("component update olundu")
+    }
+}
 artır=()=>{
     this.setState({counter : this.state.counter+1})
 }
-
   render() {
     return (
       <div>
         <h1>Class Component</h1>
         <p>Counter: {this.state.counter}</p>
+        <p>Counter2:{this.state.counter2}</p>
         <button onClick={()=>this.artır()}>Artır</button>
+        <button onClick={()=>this.setState({counter2:this.state.counter2+1})}>Artır 2</button>
       </div>
     )
   }
--- reacttask/test/src/components/funksiyacomponent.jsx
+++ reacttask/test/src/components/funksiyacomponent.jsx
@@ -1,24 +1,26 @@
 import React from 'react'
+import { useEffect } from 'react'
 import { useState } from 'react'
 const FunksiyaComponent= () => {
     const[Counter, setCounter]=useState(0)
-
+    const[Counter2, setCounter2]=useState(0)
+    
+    useEffect(()=>{
+        console.log("Yükləndi")
+        console.log("Update olundu")
+    },[Counter,Counter2])
     const azalt = () =>{
         setCounter(Counter-1)
     }
     return(
-
     <div>
             <h1>Function Component</h1>
             <p>Counter: {Counter}</p>
+            <p>Counter2: {Counter2}</p>
             <button onClick={()=>azalt()}>Azalt</button>
+            <button onClick={()=>setCounter2(Counter2-1)}>Azalt2</button>
     </div>
-
-
     )
-
-
-
 }
 
 export default FunksiyaComponent
