c8c0d8b87a2ea7dec19fdca4551ac3e4b905ccb7
GulsenZalova <zalova0202@gmail.com>
2022-06-28 21:32:00 +0400

    update

--- reacttask/newreactproject/src/components/Common.jsx
+++ reacttask/newreactproject/src/components/Common.jsx
@@ -7,34 +7,45 @@ class Common extends React.Component{
         movies:[
             {
                 "id":1,
-                "name":"Kino 1",
+                "name":"Bad Boys for life",
                 "rating":8.3,
                 "overview":"Kino 1in mezmunu",
                 "imageURL":"https://cdn1.ntv.com.tr/gorsel/vFwgb7p4AkuRTdsZS_ocFA.jpg?width=1000&mode=crop&scale=both"
             },
             {
                 "id":2,
-                "name":"Kino 2",
+                "name":"Little Women",
                 "rating":10,
                 "overview":"Kino 2in mezmunu",
-                "imageURL":"https://cdn1.ntv.com.tr/gorsel/vFwgb7p4AkuRTdsZS_ocFA.jpg?width=1000&mode=crop&scale=both"
+                "imageURL":"https://cdn1.ntv.com.tr/gorsel/0_xboVeudEyw2GqkpDoUdw.jpg?width=1000&mode=crop&scale=both"
             },
             {
                 "id":3,
-                "name":"Kino 3",
+                "name":"RamPage",
                 "rating":8,
                 "overview":"Kino 3in mezmunu",
-                "imageURL":"https://cdn1.ntv.com.tr/gorsel/vFwgb7p4AkuRTdsZS_ocFA.jpg?width=1000&mode=crop&scale=both"
+                "imageURL":"https://cdn1.ntv.com.tr/gorsel/qljKxJVP1E6XPJXobqgkrg.jpg?width=674&height=1000&mode=crop&scale=both&v=20180503042121012"
             },
             {
                 "id":4,
-                "name":"Kino 4",
+                "name":"Terminator",
                 "rating":9,
                 "overview":"Kino 4in mezmunu",
-                "imageURL":"https://cdn1.ntv.com.tr/gorsel/vFwgb7p4AkuRTdsZS_ocFA.jpg?width=1000&mode=crop&scale=both"
+                "imageURL":"https://cdn1.ntv.com.tr/gorsel/abG_4kFMnkKVhAALHnfwWg.jpg?width=1000&mode=crop&scale=both"
             },
         ]
     }
+     
+    deleteMovie=(movie)=>{
+        const newMoviesList=this.state.movies.filter(
+            m => m.id !== movie.id
+        )
+
+        this.setState({
+            movies:newMoviesList
+        })
+    }
+   
     render(){
         return (
             <div className="container"> 
@@ -43,7 +54,8 @@ class Common extends React.Component{
                         <SearchBar/>
                     </div>
                 </div>
-                <MovieList movies={this.state.movies}/>
+                <MovieList movies={this.state.movies}
+                deleteMovieProp={this.deleteMovie}/>
             </div>
         )
     }
