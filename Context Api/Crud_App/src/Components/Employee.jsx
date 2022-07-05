3dde077664cbeedac92cb601a7e3fea2cf3bf9e7
GulsenZalova <zalova0202@gmail.com>
2022-07-05 14:18:58 +0400

    layihede useState hookdan istifadə etdim

--- Context Api/Crud_App/src/Components/Employee.jsx
+++ Context Api/Crud_App/src/Components/Employee.jsx
@@ -1,13 +1,23 @@
 
-const Employee = ()=>{
+const Employee = ({employees})=>{
     return(
-    <div>
-        <ul>
-            <li>Employee 1</li>
-            <li>Employee 2</li>
-            <li>Employee 3</li>
-        </ul>
-    </div>
+        <>
+        {
+            employees.map((employee)=>(
+            <tr key={employee.id}>
+			<td>{employee.name}</td>
+			<td>{employee.email}</td>
+			<td>{employee.address}</td>
+			<td>{employee.phone}</td>
+			<td>
+				<a href="#editEmployeeModal" className="edit" data-toggle="modal"><i className="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>
+				<a href="#deleteEmployeeModal" className="delete" data-toggle="modal"><i className="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
+			</td>
+		</tr>
+        ))
+        
+        }
+        </>
     )
 }
 
