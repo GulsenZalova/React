8d04a4ea41caa8f5ab40098a669b57d5575b516c
GulsenZalova <zalova0202@gmail.com>
2022-07-05 13:05:45 +0400

    Crud project start

---
+++ Context Api/Crud_App/src/Components/Employee.jsx
@@ -0,0 +1,14 @@
+
+const Employee = ()=>{
+    return(
+    <div>
+        <ul>
+            <li>Employee 1</li>
+            <li>Employee 2</li>
+            <li>Employee 3</li>
+        </ul>
+    </div>
+    )
+}
+
+export default Employee
