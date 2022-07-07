5a1308e2155776ee3fe4ecca60f09e3bd1ab6fc9
GulsenZalova <zalova0202@gmail.com>
2022-07-07 19:59:56 +0400

    logalStorage kodları yazıldı

--- Context Api/Crud_App/src/Contexts/EmployeeContext.jsx
+++ Context Api/Crud_App/src/Contexts/EmployeeContext.jsx
@@ -1,4 +1,4 @@
-import { createContext } from "react";
+import { createContext, useEffect } from "react";
 import { useState } from "react";
 import { v4 as uuidv4 } from 'uuid';
 
@@ -13,7 +13,15 @@ const EmployeeContextProvider=(props)=>{
         {id:uuidv4(), name: 'Martin Blank', email: 'martinblank@mail.com', address: 'Via Monte Bianco 34, Turin, Italy', phone: '(480) 631-2097'}
     ])
 
-const sortedEmployees=employees.sort((a,b)=> a.name > b.name ? 1 : -1)
+  useEffect(()=>{
+    const employees=localStorage.getItem("employees")
+    setEmployees(JSON.parse(employees))
+  },[]) 
+  
+   useEffect(()=>{
+    localStorage.setItem("employees", JSON.stringify(employees))
+   })
+    const sortedEmployees=employees.sort((a,b)=> a.name > b.name ? 1 : -1)
     const addEmployees=(name,email,address,phone)=>{
         setEmployees([...employees, {id:uuidv4(),name,email,address,phone}])
     }
